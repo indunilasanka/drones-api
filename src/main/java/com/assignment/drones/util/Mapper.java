11c85058d984dc9d3e57816e371b4ac97e07e05d
asankai <waiasanka.13@cse.mrt.ac.lk>
2022-12-25 13:57:46 +0530

    mapper class with domain-dto mapping logic

---
+++ src/main/java/com/assignment/drones/util/Mapper.java
@@ -0,0 +1,69 @@
+package com.assignment.drones.util;
+
+import com.assignment.drones.dto.DroneDTO;
+import com.assignment.drones.dto.MedicationDTO;
+import com.assignment.drones.model.Drone;
+import com.assignment.drones.model.Medication;
+import com.assignment.drones.model.State;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mapper {
+    public static Drone mapToDrone(DroneDTO dto) {
+        Drone drone = new Drone();
+        drone.setSerialNumber(dto.getSerialNumber());
+        drone.setModel(dto.getModel());
+        drone.setBatteryCapacity(dto.getBatteryCapacity());
+        drone.setWeightLimit(dto.getWeightLimit());
+        drone.setState(State.IDLE);
+        return drone;
+    }
+
+    public static DroneDTO mapToDroneDTO(Drone drone) {
+        DroneDTO dto = new DroneDTO();
+        dto.setSerialNumber(drone.getSerialNumber());
+        dto.setModel(drone.getModel());
+        dto.setWeightLimit(drone.getWeightLimit());
+        dto.setBatteryCapacity(drone.getBatteryCapacity());
+        dto.setState(drone.getState());
+        return dto;
+    }
+
+    public static MedicationDTO mapToMedicationDTO(Medication medication) {
+        MedicationDTO dto = new MedicationDTO();
+        dto.setCode(medication.getCode());
+        dto.setName(medication.getName());
+        dto.setWeight(medication.getWeight());
+        dto.setImage(medication.getImage());
+        return dto;
+    }
+
+    public static Medication mapToMedication(MedicationDTO dto) {
+        Medication medication = new Medication();
+        medication.setName(dto.getName());
+        medication.setCode(dto.getCode());
+        medication.setWeight(dto.getWeight());
+        medication.setImage(dto.getImage());
+        return medication;
+    }
+
+    public static List<MedicationDTO> mapMedicationListToDTOs(List<Medication> medications) {
+        List<MedicationDTO> medicationDTOs = new ArrayList<>();
+
+        for (Medication medication : medications) {
+            medicationDTOs.add(mapToMedicationDTO(medication));
+        }
+        return medicationDTOs;
+    }
+
+    public static List<DroneDTO> mapDroneListToDTOs(List<Drone> drones) {
+        List<DroneDTO> droneDTOs = new ArrayList<>();
+
+        for (Drone drone : drones) {
+            droneDTOs.add(mapToDroneDTO(drone));
+        }
+        return droneDTOs;
+    }
+
+}
